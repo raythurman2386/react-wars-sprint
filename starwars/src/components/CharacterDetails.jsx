f7c67cf2e5fa611c019177f4e52ded1bcc0dbaeb
raythurman2386 <raymondthurman5@gmail.com>
2019-09-01 20:08:05 -0400

    basic layout of initial detail page

--- starwars/src/components/CharacterDetails.jsx
+++ starwars/src/components/CharacterDetails.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
 
 const CharacterDetails = () => {
+  // state hook for the data
+  const [character, setCharacter] = useState([])
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // effect hook to grab the data
+  // useEffect(() => {
+  //   axios
+  //     // grab data from api
+  //     .get(`https://swapi.co/api/people`)
+
+  //     // do something with that data
+  //     .then(res => setData(res.data.results))
+
+  //     // do something with the error
+  //     .catch(err => console.log(err.response))
+  //   // sync the array to what data
+  // }, [])
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
+
   return (
     <div>
-      <h1>Details</h1>
+      <h2>Details Page</h2>
     </div>
   )
 }
